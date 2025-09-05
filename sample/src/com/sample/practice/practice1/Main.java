33b9c0b5113b38217b35323b3627191894c870fb
TabetaYasuyuki <tabeta.yasuyuki@gmail.com>
2025-09-05 17:09:39 +0900

    Interrfaceを追加

--- sample/src/com/sample/practice/practice1/Main.java
+++ sample/src/com/sample/practice/practice1/Main.java
@@ -3,13 +3,19 @@ package com.sample.practice.practice1;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Main {
+public class Main implements MyInter {
 
 	public static void main(String[] args) {
 		ArrayList<String> array = new ArrayList<>(List.of("A","B","C"));
 		System.out.println(array);
 		array.add("D");
 		System.out.println(array);
+		
+		Main m = new Main();
+		m.printInter();
+		MyInter.printInterStatic();
+		
+		
 		
 
 	}
