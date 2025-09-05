83e38870fcb5b3f4d7d48667a471fc9522e7ce14
TabetaYasuyuki <tabeta.yasuyuki@gmail.com>
2025-09-05 16:50:39 +0900

    変更１

--- sample/src/com/sample/practice/practice1/Main.java
+++ sample/src/com/sample/practice/practice1/Main.java
@@ -1,11 +1,9 @@
 package com.sample.practice.practice1;
 
-import java.util.*;
-
 public class Main {
 
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+		
 		
 
 	}
