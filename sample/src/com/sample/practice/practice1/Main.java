ae58e71ed96d4548ac56670a9fb275a1268f4ba4
TabetaYasuyuki <tabeta.yasuyuki@gmail.com>
2025-09-05 16:54:46 +0900

    sub-edit

--- sample/src/com/sample/practice/practice1/Main.java
+++ sample/src/com/sample/practice/practice1/Main.java
@@ -1,9 +1,13 @@
 package com.sample.practice.practice1;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
 	public static void main(String[] args) {
-		
+		ArrayList<String> array = new ArrayList<>(List.of("A","B","C"));
+		System.out.println(array);
 		
 
 	}
