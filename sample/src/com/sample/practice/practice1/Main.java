9dfcfcd960426b92bc9c19892f65e4c030240c03
TabetaYasuyuki <tabeta.yasuyuki@gmail.com>
2025-09-05 16:43:27 +0900

    java

--- sample/src/com/sample/practice/practice1/Main.java
+++ sample/src/com/sample/practice/practice1/Main.java
@@ -1,9 +1,12 @@
 package com.sample.practice.practice1;
 
+import java.util.*;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
+		Scanner sc = new Scanner(System.in);
+		
 
 	}
 
